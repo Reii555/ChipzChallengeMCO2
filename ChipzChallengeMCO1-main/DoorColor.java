32885a6bacbb1d5df6889919ad5fc06971083aae
Reii555 <SRLagoos@gmail.com>
2025-11-17 21:09:35 +0800

    Delete ChipzChallengeMCO1-main directory
    mali e hehe

--- ChipzChallengeMCO1-main/DoorColor.java
+++
@@ -1,9 +0,0 @@
-/**
- * Enum representing the possible colors for doors in the game.
- * Each color corresponds to a specific key type required for opening.
- */
-public enum DoorColor {
-    RED,
-    BLUE
-}
-
